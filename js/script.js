e152ae01fe822079a0c29cd1864faf5fa1bdc2d7
9ine <81828210+Ermac-mein@users.noreply.github.com>
2024-11-25 13:06:05 +0100

    Delete js directory

--- js/script.js
+++
@@ -1,9 +0,0 @@
-// JavaScript to toggle the side menu visibility
-document.querySelector(".menu-icon").addEventListener("click", function() {
-    document.querySelector(".side-menu").classList.toggle("active");
-});
-
-// If you want to close the side menu when clicking on a link or the close icon
-document.querySelector(".side-menu .close-icon").addEventListener("click", function() {
-    document.querySelector(".side-menu").classList.remove("active");
-});
